3330533e89e0059b5e87550c42ede118edac8711
Alejandro Tirado <aletiradovilla@gmail.com>
2026-05-26 09:47:28 -0600

    fix: usar anon key para supabaseAdmin

--- app/supabase.ts
+++ app/supabase.ts
@@ -2,7 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://xdxqqxpfleypkhgwuguf.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkeHFxeHBmbGV5cGtoZ3d1Z3VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxMTY5MjgsImV4cCI6MjA5MzY5MjkyOH0.fLzC803pAJEwEIUZtxvQquyRHcWeb7O4rmvn6OyLcGY';
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
+export const supabaseAdmin = createClient(supabaseUrl, supabaseAnonKey);
